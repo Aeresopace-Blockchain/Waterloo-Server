cea43ef1807f40369436321a1c2e79973ab7f55f
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-06-23 20:47:32 -0400

    feat: add custom var

--- proofRequest.js
+++ proofRequest.js
@@ -3,13 +3,13 @@ const { KYCAgeCredential } = require("./vcHelpers/KYCAgeCredential");
 // design your own customised authentication requirement here using Query Language
 // https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
 
-const humanReadableAuthReason = "Must be born before this year";
+const humanReadableAuthReason = "Rafael Fuentes Rangel";
 
 const credentialSubject = {
   birthday: {
     // users must be born before this year
     // birthday is less than Jan 1, 2023
-    $lt: 20230101,
+    $lt: 20022007,
   },
 };
 
