2050298f99323d791dec99b1c0374a142c617b39
RafaBlockDev <rafafuentesrangel@outlook.com>
2023-06-24 17:50:19 -0400

    fix: change propertie name

--- vcHelpers/KYCAgeCredential.js
+++ vcHelpers/KYCAgeCredential.js
@@ -1,12 +1,12 @@
 module.exports = {
     // VC type: KYCAgeCredential
     // https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld
-    KYCAgeCredential: (credentialSubject) => ({
+    KYCPilotCredential: (credentialSubject) => ({
       id: 1,
       circuitId: "credentialAtomicQuerySigV2",
       query: {
         allowedIssuers: ["*"],
-        type: "KYCAgeCredential",
+        type: "KYCPilotCredential",
         context:
           "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
         credentialSubject,
